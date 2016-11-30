d9047d1daa614097434d738773e0a340073d8d1f
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-30 17:08:58 +0000

    Refactor 1) UpdateCalculator now uses 1 line if statements and ternary statements

--- update_calculator.rb
+++ update_calculator.rb
@@ -1,32 +1,17 @@
 class UpdateCalculator
 
   def decrease(item, is_conjured)
-    if is_conjured
-      return 2
-    end
-
-    if item.sell_in == 0
-      return 2
-    end
-    1
+    return 2 if is_conjured
+    item.sell_in == 0 ? 2 : 1
   end
 
   def increase(item)
-    if item.quality >= 50
-      return 0
-    end
-    1
+    item.quality >= 50 ? 0 : 1
   end
 
   def backstage_increase(sell_in)
-    if sell_in <= 5
-      return 3
-    end
-
-    if sell_in <= 10
-      return 2
-    end
-    1
+    return 3 if sell_in <= 5
+    sell_in <= 10 ? 2 :1
   end
 
 end
